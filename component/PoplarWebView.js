39bea08a54b7b719e08bef085b4c16eb74356c81
lvwang <lvwangbeta@gmail.com>
2016-12-25 19:11:52 +0800

    add webview

---
+++ component/PoplarWebView.js
@@ -0,0 +1,36 @@
+'use strict';
+
+import React from 'react';
+import {
+  Image,
+  StyleSheet,
+  Text,
+  Dimensions,
+  TouchableOpacity,
+  View,
+  WebView
+} from 'react-native';
+
+var NavigationBar = require('react-native-navbar');
+import BackBtn from './navbar/BackBtn';
+
+var PoplarWebView = React.createClass({
+
+  render: function() {
+    return (
+      <View style={{flex:1, backgroundColor: 'white'}}>
+        <NavigationBar style={{borderBottomWidth: 0.5, borderBottomColor: '#F3F3F3'}}
+                       title={{title: '正文'}}
+                       leftButton={<BackBtn onPress={()=>this.props.navigator.pop()}/>}/>
+        <WebView url={'http://www.dgtle.com/article-16861-1.html'}
+                 startInLoadingState={true}
+                 domStorageEnabled={true}
+                 javaScriptEnabled={true}
+        />
+      </View>
+    );
+  },
+
+});
+
+module.exports = PoplarWebView;
