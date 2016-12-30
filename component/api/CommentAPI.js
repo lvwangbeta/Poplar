e87cc6cd80e3e5b9ae51f8d58fe91be0514ef704
lvwang <lvwangbeta@gmail.com>
2016-12-30 20:21:45 +0800

    comment api

--- component/api/CommentAPI.js
+++ component/api/CommentAPI.js
@@ -2,33 +2,33 @@
 import URLConf from './URLConf';
 import Secret from '../util/Secret';
 import Md5 from '../util/Md5';
+import {getToken} from '../util/Secret';
 
 const COMMENT_URL = URLConf.API_HOST + '/comment/';
 const COMMENT_CREATE_URL = COMMENT_URL + '/create';
 
-export function getCommentsOfObject(objectType, objectID, limit, that) {
-  var sign = Md5.hex_md5(COMMENT_URL.replace(URLConf.APP_SERVER_HOST, '')+objectType+'/'+objectID+'?ts=123456&'+Secret.secret);
-  console.log('sign:' + sign);
-  var url = COMMENT_URL+objectType+'/'+objectID+'?ts=123456&sign=' + sign;
-  console.log('get comment url : ' + url);
-  var headers = {
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      'X-Auth-Token': that.props.token,
-  }};
+export function getCommentsOfObject(objectType, objectID, limit, callback) {
 
-  fetch(url, headers)
-    .then((response) => response.json())
-    .then((responseData) => {
-      console.log(responseData);
-      that.setState({
-        commentsArray: responseData.comments.slice(0, limit),
-        dataSource: that.state.dataSource.cloneWithRows(responseData.comments.slice(0, limit)),
-        loaded: true,
-      });
-    })
-    .done();
+  getToken((token) => {
+    var sign = Md5.hex_md5(COMMENT_URL.replace(URLConf.APP_SERVER_HOST, '')+objectType+'/'+objectID+'?ts=123456&'+Secret.secret);
+    console.log('sign:' + sign);
+    var url = COMMENT_URL+objectType+'/'+objectID+'?ts=123456&sign=' + sign;
+    console.log('get comment url : ' + url);
+    var headers = {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'X-Auth-Token': token,
+    }};
+
+    fetch(url, headers)
+      .then((response) => response.json())
+      .then((responseData) => {
+        console.log(responseData);
+        callback(true, responseData.comments.slice(0, limit));
+      })
+      .done();
+  });
 }
 
 export function reply(objectType, objectID, content, commentParent, that) {
