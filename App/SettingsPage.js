1dbe06fa2cbfa815e90615b729d7d84041ad3ac8
lvwang <lvwangbeta@gmail.com>
2017-02-03 21:47:35 +0800

    About

--- App/SettingsPage.js
+++ App/SettingsPage.js
@@ -11,6 +11,7 @@ import {
 
 import NavigationBar from 'react-native-navbar';
 import BackBtn from './component/navbar/BackBtn';
+import About from './About';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
@@ -26,12 +27,20 @@ var SettingsPage = React.createClass({
         title={{title: '设置'}}
         leftButton={<BackBtn onPress={()=>this.props.navigator.pop()}/>}
         />
+        <TouchableOpacity style={styles.cell} onPress={ () => {
+            this.props.navigator.push({
+              title: '关于',
+              component: About,
+              params: {navigator: this.props.navigator}
+            });
+          }
+        }>
+          <Text style={{fontSize: 16}}>关于</Text>
+        </TouchableOpacity>
         <View style={{alignItems: 'center',}}>
-          <View style={styles.logoutBtn}>
-            <TouchableOpacity onPress={()=>{this.props.navigator.pop();this.props.logout()}}>
+            <TouchableOpacity style={styles.logoutBtn} onPress={()=>{this.props.navigator.pop();this.props.logout()}}>
               <Text style={{color: 'red', fontSize: 16,}}>退出登录</Text>
             </TouchableOpacity>
-          </View>
         </View>
       </View>
     );
@@ -45,11 +54,20 @@ var styles = StyleSheet.create({
     flex:1,
     backgroundColor: 'white',
   },
-  logoutBtn: {
-    width: windowWidth-margin*2,
+  cell: {
     height: 40,
     marginTop: 10,
-    borderRadius: 3,
+    width:windowWidth,
+    paddingLeft: margin,
+    paddingRight: margin,
+    paddingTop: 10,
+    borderColor: '#F3F3F3',
+    borderWidth: 1,
+  },
+  logoutBtn: {
+    width: windowWidth,
+    height: 40,
+    marginTop: 10,
     alignItems: 'center',
     padding: 10,
     borderColor: '#F3F3F3',
