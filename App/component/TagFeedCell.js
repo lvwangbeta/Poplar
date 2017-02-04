02bbd699cc560831c651a0a3fa09fa22187983d5
lvwang <lvwangbeta@gmail.com>
2017-02-04 20:37:21 +0800

    cell style

--- App/component/TagFeedCell.js
+++ App/component/TagFeedCell.js
@@ -127,10 +127,10 @@ var styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-    marginBottom: 10,
+    //marginBottom: 10,
     paddingBottom: 10,
     backgroundColor: 'white',
-    borderTopWidth: 0.5,
+    //borderTopWidth: 0.5,
     borderBottomWidth: 0.5,
     borderColor: '#EEEEEE',
   },
